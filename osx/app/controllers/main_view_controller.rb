748026706774e5fed3a74eef98bda994a15fd3ff
seanlilmateus <seanlilmateus@yahoo.de>
2014-01-31 14:59:01 +0100

    removed non sense method

--- osx/app/controllers/main_view_controller.rb
+++ osx/app/controllers/main_view_controller.rb
@@ -7,15 +7,11 @@ module Escape
     end
 
     def viewDidLoad
-      self.view.backgroundColor = SKColor.colorWithRed(0.15, green:0.15, blue:0.3, alpha:1.0)
       ## FOR DEBUGGING pretty handy
       # self.view.showsDrawCount = true
       # self.view.showsNodeCount = true
       # self.view.showsFPS = true
-      loadStartScene
-    end
 
-    def loadStartScene
       # Configure the view
       scene = StartScene.sceneWithSize(self.view.bounds.size)
       scene.scaleMode = SKSceneScaleModeAspectFill
